9e471cc793cb894d2255d2430f14e43ccb29958c
Darla2313 <shakra176@gmail.com>
2022-09-20 14:11:50 +0200

    added basic tts support

--- bot/main.py
+++ bot/main.py
@@ -3,16 +3,21 @@
 import os
 import discord
 from dotenv import load_dotenv
+from gtts import gTTS
+import asyncio
+
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = discord.Client(discord.Intents.default())
+bot = discord.Client()
 
 
-async def load_blacklist(f: str) -> list:
+async def load_blacklist(f: str = "test") -> list:
 	with open(f) as bl:
-		return bl.read.splitlines()
+		lis = []
+		#return lis.append(bl.read.splitlines())
+	return lis
 
 
 blacklist = await load_blacklist()
@@ -35,5 +40,21 @@ async def on_message(message):
 			message.author.create_dm()
 			m = f"hi please refrain from using the word{hit} in the future"
 			await message.author.dm_channel.send(m)
+
+
+@bot.command(name="dtts", pass_context=True,)
+async def dtts(context):
+	user = context.message.author
+	out_channel = await user.voice.voice_channel.connect()
+
+	txt = context.message.content
+	conv = gTTS(text=txt, lang="en", slow=False)
+	conv.save("conv.mp3")
+	audio_out = out_channel.create_ffmep_player("conv.mp3")
+	audio_out.start()
+	while not audio_out.is_done():
+		await asyncio.sleep(10)
+	audio_out.stop()
+	os.remove("conv.mp3")
 
 bot.run(TOKEN)
